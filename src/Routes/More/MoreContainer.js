38e437c06bc177dc4fc83c28641b0165b92587cd
CheSangJung <shtingstar@hanmail.net>
2021-07-27 17:52:28 +0900

    more

--- src/Routes/More/MoreContainer.js
+++ src/Routes/More/MoreContainer.js
@@ -1,15 +1,9 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React from "react";
 import MorePresenter from "./MorePresenter";
-import { moviesApi } from "api";
 
-const MoreContainer = ({location}) => {
-    
-
+const MoreContainer = () => {
     return (
-        <div>
-            <button onClick={()=>{onMorePage()}}>more</button>
-        </div>
-        
+        <MorePresenter />
     )
 }
 
